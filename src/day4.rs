7e4dcfe830fab3c026058770b448c41c7ee8b260
Henry Morin <morinh@rose-hulman.edu>
2023-12-05 20:27:10 -0500

    formatting

--- src/day4.rs
+++ src/day4.rs
@@ -40,7 +40,41 @@ fn part1(file_path: String) {
     println!("Part 1:\n{}", total.to_string());
 }
 
-fn part2(_file_path: String) {}
+fn part2(file_path: String) {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let mut mult: Vec<u32> = vec![1; contents.lines().count()];
+
+    for (i, line) in contents.lines().enumerate() {
+        let mut game_total: u32 = 0;
+        let split_line: Vec<&str> = line.split_terminator("|").collect();
+        let winning: Vec<u32> = split_line[0].split_terminator(":").collect::<Vec<&str>>()[1]
+            .trim()
+            .split_terminator(" ")
+            .collect::<Vec<&str>>()
+            .iter()
+            .filter(|x| x != &&"")
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        let actual: Vec<u32> = split_line[1]
+            .trim()
+            .split_terminator(" ")
+            .collect::<Vec<&str>>()
+            .iter()
+            .filter(|x| x != &&"")
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        actual.iter().for_each(|x| {
+            if winning.contains(x) {
+                game_total += 1;
+            }
+        });
+        for x in i + 1..=i + (game_total as usize) {
+            mult[x] += 1 * mult[i];
+        }
+    }
+    println!("Part 2:\n{}", mult.iter().sum::<u32>().to_string());
+}
 
 pub fn main() {
     println!("DAY 4:");
